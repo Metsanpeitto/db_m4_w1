ee2f3ba42361a62496b3c28fa61b73ef3f6af6d7
Metsanpeitto <abraham@waldenberginc.com>
2021-09-21 00:22:57 +0200

    Add the last queries

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    id INT AUTO_INCREMENT,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
@@ -10,6 +10,36 @@ CREATE TABLE animals (
     weight_kg DECIMAL(3) NOT NULL
 );
 
-/* task-2 */
-/* Add a column species of type string to your animals table. Modify your schema.sql file. */
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+/* Create a table named owners with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+full_name: string
+age: integer   */
+
+CREATE TABLE owners (
+    id INT AUTO_INCREMENT,
+    full_name varchar(100),
+    age INT,
+    PRIMARY KEY(id)
+    );
+
+/* Create a table named species with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string */
+
+CREATE TABLE species (
+    id INT AUTO_INCREMENT,
+    name varchar(100),    
+    PRIMARY KEY(id)
+    );
+
+
+/* Remove column species */
+ALTER TABLE animals DROP COLUMN species;
+/*  Add column species_id which is a foreign key referencing species table  */
+ALTER TABLE animals ADD  species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+/* Add column owner_id which is a foreign key referencing the owners table */
+ALTER TABLE animals ADD  owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
